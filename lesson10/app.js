110d0fb4572c00ba487b32a7414928c7102c7d81
Eduardo Jaime <eduardojaime86@gmail.com>
2021-11-23 17:10:47 -0500

    lesson10 demo files

---
+++ lesson10/app.js
@@ -0,0 +1,125 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+// Import the mongoose module and globals file
+const mongoose = require('mongoose');
+const config = require('./config/globals');
+
+// Securing the API
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+
+// Documenting the API
+const swaggerUi = require('swagger-ui-express');
+
+var indexRouter = require('./routes/index');
+const projectsRouter = require('./routes/api/projects');
+// create router object for v2
+const projectsRouterV2 = require('./routes/api/v2/projects');
+
+var app = express();
+
+// 1) Load from YAML file
+// const YAML = require('yamljs');
+// const swaggerDocument = YAML.load('./documentation/api-specification.yaml');
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// 2) Load from swagger-jsdoc comments
+// Initialize swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerJSDoc = require('swagger-jsdoc');
+// Configure the options object
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Projects Manager API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./routes/api/*.js'], // files containing annotations as above
+};
+// Create a Spec object
+const swaggerSpec = swaggerJSDoc(options);
+// Use it as the base OAS
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// 3) Load from URL
+// const options = {
+//   swaggerOptions: {
+//     url: 'http://petstore.swagger.io/v2/swagger.json'
+//   }
+// }
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, options));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// Initialize passport module
+app.use(passport.initialize());
+
+// Implement basic strategy
+passport.use(new BasicStrategy((username, password, done) => {
+  // For now let's hardcode username and password
+  // admin:default encoded as YWRtaW46ZGVmYXVsdA==
+  // admin:incorrectpswd encoded as YWRtaW46aW5jb3JyZWN0cHN3ZA==
+  if (username == 'admin' && password == 'default') {
+    console.log(`User ${username} authenticated successfully!`)
+    return done(null, username);
+  }
+  else {
+    console.log(`Authentication failed for user ${username}`)
+    return done(null, false);
+  }
+  /*
+    // Example using MongoDB model
+    // taken from https://github.com/jaredhanson/passport-http
+    User.findOne({ username: userid }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  */
+}));
+
+app.use('/', indexRouter);
+// Best practice is to put all API related endpoints in their own section
+app.use('/api/projects', passport.authenticate('basic', { session: false }), projectsRouter);
+// Add version specific endpoints
+app.use('/api/v1/projects', passport.authenticate('basic', { session: false }), projectsRouter);
+app.use('/api/v2/projects', passport.authenticate('basic', { session: false }), projectsRouterV2);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+//Connect to Mongo DB after all controller/router configuration
+mongoose.connect(config.db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((message) => {
+    console.log('Connected successfully!');
+  })
+  .catch((error) => {
+    console.log(`Error while connecting! ${reason}`);
+  })
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ lesson10/routes/api/projects.js
@@ -0,0 +1,167 @@
+const express = require('express');
+const router = express.Router();
+// Import Project model
+const Project = require('../../models/project');
+// Set a default page size to use in any request handler in this route
+const pageSize = 10;
+
+
+
+// GET /projects
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: Lists all projects
+ *     responses:
+ *       200:
+ *         description: Returns an unfiltered list of projects
+ */
+router.get('/', (req, res, next) => {
+    // query parameter-based versioning
+    // expecting a single digit number
+    // needs to be defined in the API specification document
+    let version = req.query.v || '1';
+
+    switch (version) 
+    {
+        case '1':
+            GetProjectsV1(req, res, next);
+            break;
+        case '2':
+            GetProjectsV2(req, res, next);
+            break;
+        default:
+            res.status(400).json({ 'ErrorMessage': 'Version not supported' });
+            break
+    }
+    
+});
+
+function GetProjectsV1(req, res, next) {
+// Filter the list via optional URL query string parameters
+    // using the req.query object
+    // Expected query string ?status=STARTED&course=WebAPI
+    
+    // create variable for storing page number
+    // extract value from query string
+    // Expected ?page=1
+    let page = req.query.page || 1; // default to page 1 
+    // calculate how many records to skip
+    // page 1 shows records 1 to 10 so skip 0
+    // page 2 shows records 11 to 20 so skip 10
+    let skipSize = pageSize * (page - 1);
+
+    // create empty query object
+    let query = {};
+    // if course is included in request, then add it to query object
+    if (req.query.course)
+    {
+        query.course = req.query.course;
+    }
+    // if status is included in request, then add it to query object
+    if (req.query.status)
+    {
+        query.status = req.query.status;
+    }
+
+    // Modify find() to accept query
+    Project.find(
+        query, // filter 
+        (err, projects) => { // callback
+            if (err) {
+                console.log(err);
+                res.status(500).json({ 'ErrorMessage': 'Server threw an exception' });
+
+            }
+            else {
+                res.status(200).json(projects);
+            }
+        })
+        // implement pagination
+        .sort({ name: 1 }) // to achieve a consisten result sort by name A to Z
+        .limit(pageSize) // set page size limit
+        .skip(skipSize);
+}
+
+function GetProjectsV2(req, res, next) {
+    res.status(200).json({ 'Message': 'Work in progress!' });
+}
+
+// POST /projects
+router.post('/', (req, res, next) => {
+    // Test first by logging and sending back body
+    // console.log(req.body);
+    // res.json(req.body);
+
+    // Validate required fields
+    if (!req.body.name) {
+        res.status(400).json({ 'ValidationError': 'Name is a required field' });
+    }
+    else if (!req.body.course) {
+        res.status(400).json({ 'ValidationError': 'Course is a required field' });
+    }
+    else {
+        // s
+        Project.create({
+            name: req.body.name,
+            dueDate: req.body.dueDate,
+            course: req.body.course
+        }, (err, newProject) => {
+            // implement error handling logic
+            if (err) {
+                console.log(err);
+                res.status(500).json({ 'ErrorMessage': 'Server threw an exception' });
+            }
+            else {
+                res.status(200).json(newProject);
+            }
+        });
+    }
+});
+
+// PUT /projects/:_id
+router.put('/:_id', (req, res, next) => {
+    // Validate required fields
+    if (!req.body.name) {
+        res.status(400).json({ 'ValidationError': 'Name is a required field' });
+    }
+    else if (!req.body.course) {
+        res.status(400).json({ 'ValidationError': 'Course is a required field' });
+    }
+    else {
+        Project.findOneAndUpdate(
+            { _id: req.params._id }, // filter query
+            {
+                name: req.body.name,
+                dueDate: req.body.dueDate,
+                course: req.body.course,
+                status: req.body.status
+            }, // update document
+            (err, updatedProject) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).json({ 'ErrorMessage': 'Server threw an exception' });
+                }
+                else {
+                    res.status(200).json(updatedProject);
+                }
+            } // update callback 
+        );
+    }
+});
+
+// DELETE /projects/:_id
+router.delete('/:_id', (req, res, next) => {
+    Project.remove({ _id: req.params._id }, (err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ 'ErrorMessage': 'Server threw an exception' });
+        }
+        else {
+            res.status(200).json({ 'success': 'true' });
+        }
+    });
+});
+
+module.exports = router;
