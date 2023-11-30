75bf11fcc3cef6e21175ae3d7f54297b03cdf89d
Eduardo Jaime <eduardojaime86@gmail.com>
2023-11-29 20:18:34 -0500

    l11 part 1 enable cors

--- demos/lesson11/server/app.js
+++ demos/lesson11/server/app.js
@@ -52,7 +52,7 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.use(cors());
+app.use(cors()); // enabled for all origins
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
