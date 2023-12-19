f082b917d305bebd58f1c491ce0c22a810526bae
Eduardo Jaime <eduardojaime86@gmail.com>
2023-12-19 11:09:56 -0500

    l12 code

---
+++ demos/lesson12/src/functions/addfunctiontest01.js
@@ -0,0 +1,12 @@
+const {app} = require("@azure/functions");
+
+app.http("addfunctiontest01", {
+    methods: ["GET", "POST"],
+    authLevel: "anonymous",
+    handler: async (request, context) => {
+        const x = request.query.get("x") || "0";
+        const y = request.query.get("y") || "0";
+        let result = x + y;
+        return { body: `Result of ${x} + ${y} is ${result}` };
+    }
+});
