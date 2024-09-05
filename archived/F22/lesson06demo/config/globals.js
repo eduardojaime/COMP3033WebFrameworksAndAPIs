c7a344ab42775623d91b3e769123110169631e24
Eduardo Jaime <eduardojaime86@gmail.com>
2024-09-05 19:01:04 -0400

    archived older demos

--- archived/F22/lesson06demo/config/globals.js
+++
@@ -1,6 +0,0 @@
-// Global configurations file
-const configurations = {
-    'db': 'mongodb+srv://admin:comp3033strongpass2021@cluster0.86msx.mongodb.net/comp3033'
-}
-
-module.exports = configurations;
--- archived/F23/lesson04/config/globals.js
+++
@@ -1,5 +0,0 @@
-// configuration object > JSON format
-const configuration = {
-    "db": "mongodb+srv://webappdbuser:nkYWAR2AUgfWyH08@cluster0.ho53tdt.mongodb.net/ProjectTrackerAPI"
-}
-module.exports = configuration;
