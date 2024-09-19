56ab4ae4966558ff6871bc30343f3d43944653e3
Eduardo Jaime <eduardojaime86@gmail.com>
2024-09-18 20:39:08 -0400

    l03 part 1 /api/projects endpoint

--- demos/lesson03/app.js
+++ demos/lesson03/app.js
@@ -5,22 +5,28 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var projectsRouter = require('./routes/api/projects');
+// var usersRouter = require('./routes/users');
+// Object that represents the express application
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// Routing Table (rules)
+// MVC routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// API routes
+// BEST PRACTICE: Prefix all API routes with /api to avoid conflicts 
+// and distinguish between API and MVC routes
+app.use("/api/projects", projectsRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- demos/lesson03/routes/index.js
+++ demos/lesson03/routes/index.js
@@ -1,3 +1,4 @@
+// This is a MVC Controller
 var express = require('express');
 var router = express.Router();
 
